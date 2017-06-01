f2bbef3bbd3f1066e09fcc72a4d86e147dbbc289
Travis Gray <travis.gray@clearcapital.com>
2017-06-01 13:55:44 -0700

    working starting point for web view with frame!

--- App.js
+++ App.js
@@ -1,112 +1,58 @@
-import React, {Component} from 'react';
-import {StyleSheet, Text, View, Dimensions, Alert} from 'react-native';
-import Expo from 'expo'
-import * as THREE from 'three'
+import React, {Component} from 'react'
+import ReactDOMServer from 'react-dom/server'
+import {StyleSheet, Text, View, Dimensions, Alert, WebView} from 'react-native'
+import webapp from './webapp/index.html'
 
-console.ignoredYellowBox = ['THREE']
-
-const {width, height} = Dimensions.get('window')
-const {
-  BoxGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  Scene,
-  DataTexture
-} = THREE
-
-const ThreeView = Expo.createTHREEViewClass(THREE)
 export default class App extends Component {
-  meshes = []
-
-  componentWillMount () {
-    this.setupScene()
-    this.loadTestContent()
-    this.loadPano()
+  state = {
+    imgUrl: '360image.JPG'
   }
 
-  setupScene () {
-    this.scene = new Scene()
-    this.scene.name = 'scene'
-
-    this.camera = new PerspectiveCamera(75, width / height, .01, 1000)
-    this.camera.name = 'perspective-camera'
-    this.camera.target = new THREE.Vector3(0, 0, 0)
-  }
-
-  createMesh (type) {
-    return new Mesh(
-      new BoxGeometry(3, 3, 3),
-      new MeshBasicMaterial({
-        color: 0x00ff00
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({
+        imgUrl: '360image2.JPG'
       })
-    )
+    }, 5000)
   }
 
-  loadTestContent () {
-    let meshCount = 6
+  componentDidUpdate () {
+    const template = this.updateTemplate()
 
-    while (meshCount) {
-      const mesh = this.createMesh()
-      mesh.position.set(0, -15 + meshCount * 5, -25)
-      mesh.lookAt(this.camera.position)
-      this.scene.add(mesh)
-      this.meshes.push(mesh)
-      meshCount--
-    }
+    this.webview.injectJavaScript(`
+      var container = document.querySelector('.template')
+      container.innerHTML = '${template}'
+    `)
   }
 
-  async loadPano () {
-    console.log('loadPano')
-    global.document = {}
-    const {ThreeView} = this
-
-    const panoAsset = Expo.Asset.fromModule(require('./360-img.jpg'))
-    await panoAsset.downloadAsync()
-    console.log('panoAsset', panoAsset)
-
-    const panoGeometry = new THREE.SphereGeometry(100, 60, 40)
-    panoGeometry.applyMatrix(new THREE.Matrix4().makeScale(-1, 1, 1))
-    console.log('panoGeometry', panoGeometry)
-
-    // https://threejs.org/docs/index.html#api/textures/DataTexture
-    const texture = new DataTexture(panoAsset.uri, panoAsset.width, panoAsset.height)
-    texture.magFilter = THREE.NearestFilter
-    texture.minFilter = THREE.NearestFilter
-    texture.needsUpdate = true
-    texture.isDataTexture = true
-    console.log('texture', texture)
-
-    const panoMaterial = new MeshBasicMaterial({
-      map: texture,
-      transparent: true,
-      fog: false
-    })
-    console.log('panoMaterial', panoMaterial)
-
-    const panoMesh = new THREE.Mesh(panoGeometry, panoMaterial)
-    panoMesh.material.side = THREE.BackSide
-    panoMesh.name = 'pano'
-    console.log('panoMesh', panoMesh)
-
-    this.scene.add(panoMesh)
+  handleMessage(evt) {
+    console.log('evt.nativeEvent.data: ', evt.nativeEvent.data)
   }
 
-  update = (dt) => {
-    this.meshes.forEach((mesh) => {
-      mesh.rotation.x += 1 * dt
-      mesh.rotation.y += 2 * dt
-    })
+  updateTemplate() {
+    const {imgUrl} = this.state
+    const markup = [
+      `<a-scene antialias="true">`,
+        `<a-entity scale="1 1 -1" material="shader: flat; src: ${imgUrl}" id="sky" geometry="primitive: sphere; radius: 100" />`,
+      `</a-scene>`
+    ]
+
+    return markup.join('')
   }
 
   render() {
+    const template = this.updateTemplate()
+
     return (
-      <ThreeView
-        style={{flex: 1}}
-        backgroundColor={'#111111'}
-        scene={this.scene}
-        camera={this.camera}
-        tick={this.update}
+      <WebView
+        ref={webview => {this.webview = webview}}
+        source={webapp}
+        injectedJavaScript={`
+          var container = document.querySelector('.template')
+          container.innerHTML = '${template}'
+        `}
+        style={{marginTop: 20}}
+        onMessage={this.handleMessage}
       />
     )
   }
---
+++ webapp/main.js
@@ -0,0 +1,5 @@
+var AFRAME = window.AFRAME
+
+function handleClick() {
+  window.postMessage(AFRAME.components)
+}
